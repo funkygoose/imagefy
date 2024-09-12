344541ae7389cda25a9885dbf67f96ddcfaf34d6
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2024-09-11 22:33:07 -0700

    initial commit

---
+++ components/shared/Sidebar.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+        <div className="flex size-full flex-col gap-4">
+            <Link href="/" className="sidebar-logo">
+            <Image src="/public/assets/images/logo-text.svg" alt="logo" width={180} height={28} />
+            </Link>
+        </div>
+        
+
+    </aside>
+  )
+}
+
+export default Sidebar
