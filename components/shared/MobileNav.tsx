81ecccd2166373f111321166675a3baa22907fba
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2024-09-23 22:51:16 -0700

    initial commit

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import {
     Sheet,
@@ -10,11 +12,15 @@ import {
 import Link from 'next/link'
 import Image from 'next/image'
 import { SignedIn, UserButton } from '@clerk/nextjs'
+import { navLinks } from '@/constants'
+import { usePathname } from 'next/navigation'
 
 const MobileNav = () => {
+
+    const pathname = usePathname()
     return (
         <header className="header">
-            <Link href="/" className="flex items-center gap-2 md-py-2">
+            <Link href="/" className="flex items-center gap-2 md:py-2">
                 <Image src="/assets/images/logo-text.svg"
                     alt='logo'
                     width={180}
@@ -28,17 +34,39 @@ const MobileNav = () => {
                     <Sheet>
                         <SheetTrigger>
                             <Image src="/assets/icons/menu.svg"
-                            alt="menu" 
-                            width={32}
-                            height={32}
-                            className="cursor-pointer"/>
+                                alt="menu"
+                                width={32}
+                                height={32}
+                                className="cursor-pointer" />
                         </SheetTrigger>
                         <SheetContent className="sheet-content sm:w-64">
                             <SheetContent className="sheet-content sm:w-64">
-                                <Image src="/assets/images/logo-text.svg" 
-                                alt="logo"
-                                width={152}
-                                height={23}/>
+                                <Image src="/assets/images/logo-text.svg"
+                                    alt="logo"
+                                    width={152}
+                                    height={23} />
+                                <ul className="header-nav_elements">
+                                    {navLinks.slice(0, 6).map((link) => {
+                                        const isActive = link.route === pathname
+
+                                        return (
+                                            <li key={link.route}
+                                                className={` ${isActive && 'gradient-text'} p-18 flex whitespace-nowrap text-dark-700`}>
+
+                                                <Link className="sidebar-link" href={link.route}>
+                                                    <Image
+                                                        src={link.icon}
+                                                        alt="logo"
+                                                        width={24}
+                                                        height={24}
+                                                        className={`${isActive && 'brightness-200'}`}
+                                                    />
+                                                    {link.label}
+                                                </Link>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
                             </SheetContent>
                         </SheetContent>
                     </Sheet>
