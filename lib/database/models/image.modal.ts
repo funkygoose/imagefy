5cf9d711cca665638392c696e03ef52e521a7c0c
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2024-10-14 22:13:48 -0700

    initial commit

--- lib/database/models/image.modal.ts
+++ lib/database/models/image.modal.ts
@@ -1,7 +1,32 @@
 import { transformationTypes } from "@/constants";
-import { model, models, Schema } from "mongoose";
+import { Document, model, models, Schema } from "mongoose";
 
-const ImageSchema = new Schema({
+import { ObjectId } from "mongoose";
+
+export interface IImage extends Document {
+    title: string;
+    transformationType: string;
+    publicId: string;
+    secureUrl: string;
+    width?: number;
+    height?: number;
+    config?: object;
+    transformationUrl?: string;
+    aspectRatio?: string;
+    color?: string;
+    prompt?: string;
+    author: {
+        _id: string;
+        firstName: string;
+        lastName: string;
+    }
+
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+
+const ImageSchema = new Schema ({
     title: { type: String, required: true },
     transformationType: { type: String, required: true },
     publicId: { type: String, required: true },
---
+++ lib/database/models/user.modal.ts
@@ -0,0 +1,43 @@
+import { Schema,  model,  models } from "mongoose";
+
+const UserSchema = new Schema({
+    clerkId: {
+        type: String,
+        required: true,
+        unique: true,
+    }, 
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    }, 
+    photo: {
+        type: String,
+        required: true,
+    }, 
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    }, 
+    firstName: {
+        type: String,
+    }, 
+    lastName: {
+        type: String,
+    }, 
+    planId: {
+        type: String,
+        required: true,
+        default: 1,
+    }, 
+    creditBalance: {
+        type: Number,
+        default: 10,
+    }, 
+
+})
+
+const User = models?.Image || model('User', UserSchema);
+
+export default User;
