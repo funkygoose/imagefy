7005346fdf8ffd61f045ebb18ed0323a3654bb0f
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2024-11-02 23:36:27 -0700

    initial commit

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 import  { clerkClient }  from "@clerk/nextjs/server";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { users } from "@clerk/clerk-sdk-node";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
@@ -74,7 +75,7 @@ export async function POST(req: Request) {
 
     // Set public metadata
     if (newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
+      await users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
         },
